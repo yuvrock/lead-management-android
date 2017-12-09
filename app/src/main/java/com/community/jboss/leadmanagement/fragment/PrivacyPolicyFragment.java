014933d87f985609b7af6b05183c223c1f9571c3
Thomas <thomas.vayne.alderson@gmail.com>
2017-12-09 15:59:20 +0000

    GCI: Added Navigation Drawer (#13)

---
+++ app/src/main/java/com/community/jboss/leadmanagement/fragment/PrivacyPolicyFragment.java
@@ -0,0 +1,20 @@
+package com.community.jboss.leadmanagement.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.community.jboss.leadmanagement.R;
+
+public class PrivacyPolicyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_privacy_policy, container, false);
+    }
+}
